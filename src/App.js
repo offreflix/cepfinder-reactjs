0186810e56c969920c5f0b14906bd4173fa81f20
João Victor <reflixplayshd@gmail.com>
2022-03-08 15:04:39 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import './App.css';
+import swal from 'sweetalert';
+
+import api from './services/api';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  function verification(response) {
+    if (Object.keys(response.data).length === 1) {
+      // alert('Verifique o campo preenchido e tente novamente!');
+      swal(
+        'O CEP que você inseriu não existe',
+        'Cheque se digitou errado e tente novamente',
+        'error'
+      );
+    }
+  }
+
+  async function handleSearch() {
+    if (input === '') {
+      console.log('Insira um CEP!');
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      // if ((response.data.erro = true)) {
+      //   return;
+      // }
+      setCep(response.data);
+      setInput('');
+      verification(response);
+    } catch (err) {
+      console.log(err);
+      setInput('');
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Buscador CEP</h1>
+      <h1 className="title">Buscador CEP</h1>
+
       <div className="containerInput">
-        <input type="text" placeholder="Digite o CEP..."></input>
-        <button className="buttonSearch" onClick={() => {}}>
+        <input
+          type="text"
+          placeholder="Digite o CEP..."
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color={'#FFF'} />
         </button>
       </div>
-      <main>
-        <h2>CEP: 90909090</h2>
-        <span>Rua Grounded</span>
-        <span>Complemento: Sim</span>
-        <span>Vila Não</span>
-        <span>Algum Lugar - SM</span>
-      </main>
+      {Object.keys(cep).length > 1 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
